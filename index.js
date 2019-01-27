a717ec22ed892b5f8b2c7036189ae8990ed98613
Felipe <felipegte@gmail.com>
2019-01-27 21:23:42 +0000

    Adding scope to the result

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 module.exports = function(content) {
+
   this.cacheable && this.cacheable();
-  this.value = content;
 
   // Escape backticks and backslashes: “`” ⇒ “\`”, “\” ⇒ “\\”
   var escapedContent = content.replace(/[`\\]/g, function(match) {
     return "\\" + match;
   });
 
-  return "import css from 'styled-jsx/css';\n\nexport default css`" + escapedContent + "`;";
+  var scope = ""; /// Scoped
+  if (escapedContent.startsWith("/* @styled-jsx=global */")) {
+    scope = ".global";
+  }
+  
+  return "import css from 'styled-jsx/css';\n\nexport default css" + scope + "`" + escapedContent + "`;";
+
 };
